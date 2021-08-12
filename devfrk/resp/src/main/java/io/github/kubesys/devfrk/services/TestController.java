3850b444d0ca47af17fdec01f2dd945323084152
Heng WU <wuheng@iscas.ac.cn>
2021-08-12 09:51:43 +0800

    update case

--- devfrk/resp/src/main/java/io/github/kubesys/devfrk/services/TestController.java
+++ devfrk/resp/src/main/java/io/github/kubesys/devfrk/services/TestController.java
@@ -15,15 +15,57 @@ import javax.devfrk.http.httpserver.HttpContext;
 public class TestController {
 	
 	@RequestMapping(value= "/getHello")
+	@ResponseBody
 	public Object getHello(String name, int age) throws Exception {
+		if (name.equals("mary")) {
+			return name + ":" + age;
+		}
+		if (!name.equals("henry")) {
+			return new User(name, age);
+		}
 		throw new Exception("support later");
+	}
+	
+	@RequestMapping(value= "/sayHello")
+	@ResponseBody
+	public void sayHello(String name, int age) throws Exception {
 	}
 	
 	
 	@ExceptionHandler
 	@ResponseBody
 	public String invalidResponse(HttpContext r, Exception e) throws Exception {
-		return r.getPath()+ ":" + e.getLocalizedMessage();
+		return r.getPath()+ ":" + e;
+	}
+	
+	public static class User {
+		
+		protected String name;
+		
+		protected int age;
+		
+		public User(String name, int age) {
+			super();
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
 	}
 	
 }
